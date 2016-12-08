96a01f387c6edff4afa47bf0749c28f553e3709d
Haoran Yu <haoran@Haorans-Mac-mini-2.local>
2016-12-08 15:44:54 +0800

    write seeds into files

--- CUBE/multirun_test/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in {1..5}; do
-sed -i '16cprint*,\"Test'''$i'''\"' test.f90 # replace the 16th line of test.f90
-gfortran test.f90 -o test
-./test
-done
-sed -i '16cprint*,\"Test1\"' test.f90 # resetting the 16th line of test.f90
