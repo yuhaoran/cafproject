a008b9e78e8e445595a50c78c031f7fd04c7aa9a
yuhaoran <haoran@cita.utoronto.ca>
2017-02-20 02:31:21 -0500

    parallel: IC -- use real(8) for xq, so total particle number is kept over nodes

--- CUBE/utilities/initial_conditions.f90
+++ CUBE/utilities/initial_conditions.f90
@@ -1,8 +1,8 @@
-#define ZA
 #define mkdir
-!#define PID
+#define PID
 !#define READ_SEED
 !#define READ_NOISE
+!#define DO_2LPT
 
 program initial_conditions
   use pencil_fft
@@ -14,6 +14,9 @@ program initial_conditions
   ! nc: coarse grid per node per dim
   ! nf: fine grid per node per dim, ng=nf
   ! nyquest: Nyquest frequency
+  logical,parameter :: correct_kernel=.true.
+  logical,parameter :: write_potential=.true.
+
   integer,parameter :: nf_global=nf*nn
   integer,parameter :: nyquest=nf_global/2
   real,parameter :: a=1/(1+z_i)
@@ -21,10 +24,11 @@ program initial_conditions
   integer,parameter :: nk=nk_tf   !1000
   integer i,j,k,seedsize
   real kmax,temp_r,temp_theta,pow,phi8,temp8[*]
-  real(8) v8, norm
+  real(8) v8, norm, xq(3),gradphi(3)
   integer(int64) :: time64
 
-  integer nplocal
+  integer nplocal[*]
+  integer(8) npglobal
   ! power spectrum arrays
   real, dimension(7,nk) :: tf    !CAMB
   real, dimension(2,nc) :: pkm,pkn
@@ -39,11 +43,10 @@ program initial_conditions
 
   complex cx_temp(nyquest+1,nf,npen)
   real phi(-nfb:nf+nfb+1,-nfb:nf+nfb+1,-nfb:nf+nfb+1)[*]
-#ifndef ZA
+#ifdef DO_2LPT
     real phi1(-nfb:nf+nfb+1,-nfb:nf+nfb+1,-nfb:nf+nfb+1)[*]
     real,dimension(nf,nf,nf) :: phixx,phiyy,phizz,phixy,phiyz,phizx
 #endif
-  logical,parameter :: correct_kernel=.true.
 
 
   ! zip arrays
@@ -59,7 +62,7 @@ program initial_conditions
 #ifdef PID
     integer(2) pid(4,npmax)
 #endif
-  real grad_max(3),vmax(3),v_i2r(3),vf
+  real grad_max(3)[*],vmax(3),v_i2r(3),vf
 
   character (10) :: img_s, z_s
 
@@ -74,6 +77,7 @@ program initial_conditions
     print*, 'on',nn**3,' images'
     print*, 'Resolution', ng*nn
     print*, 'Number of particles per side', np_nc*nc*nn
+    print*, 'Box size', box
     print*, 'np_2n3 =',np_2n3
     print*, 'output: ', opath
     print*, 'head image number',icx,icy,icz
@@ -237,9 +241,10 @@ program initial_conditions
   !write(11) xi
   !close(11)
 
+  ! delta_field ----------------------------------------------------
+  if (head) print*, 'delta_field'
   if (head) print*, 'Start ftran'
   call pencil_fft_forward
-  ! delta field
   if (head) print*, 'Wiener filter on white noise'
   do k=1,npen
   do j=1,nf
@@ -275,7 +280,8 @@ program initial_conditions
   write(11) r3/Dgrow(a)
   close(11)
 
-  ! potential field
+  ! Potential field ----------------------------------------------------
+  if (head) print*, 'Potential field'
   do k=1,npen
   do j=1,nf
   do i=1,nyquest+1
@@ -298,6 +304,7 @@ program initial_conditions
   sync all
 
   if (correct_kernel) then
+    if (head) print*, 'correct kernel'
     call pencil_fft_backward
     !open(11,file='laplace.dat',status='replace',access='stream')
     !write(11) r3
@@ -324,6 +331,7 @@ program initial_conditions
     if (head) print*,'phi8 =',phi8
     sync all
     ! Construct Ewald potential kernel in real space
+    if (head) print*, 'Construct Ewald potential kernel in real space'
     do k=1,nf
     do j=1,nf
     do i=1,nf
@@ -355,16 +363,291 @@ program initial_conditions
   call pencil_fft_backward
   phi=0
   phi(1:nf,1:nf,1:nf)=r3 ! phi1
+  if (head) print*, 'Write phi1 into file'
   open(11,file=ic_name('phi1'),status='replace',access='stream')
   write(11) r3
   close(11)
-  call pencil_fft_forward
-  !!!!!!
-!stop
 
-#ifndef ZA
-    ! 2LPT
+!!!! DEBUG ! read same phi
+!phi=0
+!open(11,file=ic_name('phi1'),access='stream')
+!read(11) phi(1:nf,1:nf,1:nf)
+!close(11)
+!!!! ENDDEBUG
 
+  !call pencil_fft_forward
+
+#ifdef DO_2LPT
+    call correct_2lpt
+#endif
+  sync all
+
+  ! buffer phi ---------------------------------------------------
+  if (head) print*, 'Buffer phi'
+  phi(:0,:,:)=phi(nf-nfb:nf,:,:)[image1d(inx,icy,icz)]
+  phi(nf+1:,:,:)=phi(1:nfb+1,:,:)[image1d(ipx,icy,icz)]
+  sync all
+  phi(:,:0,:)=phi(:,nf-nfb:nf,:)[image1d(icx,iny,icz)]
+  phi(:,nf+1:,:)=phi(:,1:nfb+1,:)[image1d(icx,ipy,icz)]
+  sync all
+  phi(:,:,:0)=phi(:,:,nf-nfb:nf)[image1d(icx,icy,inz)]
+  phi(:,:,nf+1:)=phi(:,:,1:nfb+1)[image1d(icx,icy,ipz)]
+  sync all
+
+  if (head) print*, 'Destroying FFT plans'
+  call destroy_penfft_plan
+  sync all
+
+  ! zip checkpoints ------------------------------------------------
+  if (head) print*, 'zip checkpoints'
+  vf=vfactor(a)
+  if (head) print*, 'vf',vf
+  grad_max(1)=maxval(abs(phi(0:nf-1,1:nf,1:nf)-phi(2:nf+1,1:nf,1:nf)))
+  grad_max(2)=maxval(abs(phi(1:nf,0:nf-1,1:nf)-phi(1:nf,2:nf+1,1:nf)))
+  grad_max(3)=maxval(abs(phi(1:nf,1:nf,0:nf-1)-phi(1:nf,1:nf,2:nf+1)))
+  sync all
+  !print*, grad_max; sync all
+  do i=1,nn**3 ! co_max
+    grad_max=max(grad_max,grad_max(:)[i])
+  enddo
+  !print*, grad_max
+  vmax=grad_max/2/(4*pi)*vf
+  v_i2r=vmax*v_resolution
+  if (head) then
+    print*, 'grad_max',grad_max
+    print*, 'max dsp',grad_max/2/(4*pi)
+    print*, 'vmax',vmax
+    if (maxval(grad_max)/2/(4*pi)>=nfb) then
+      print*, 'particle dsp > buffer'
+      stop
+    endif
+  endif
+    
+  sync all
+
+  ! create particles (no communication) ----------------------------
+  if (head) print*, 'Create particles'
+  open(10,file=ic_name('zip0'),status='replace',access='stream')
+  open(11,file=ic_name('zip1'),status='replace',access='stream')
+  open(12,file=ic_name('zip2'),status='replace',access='stream')
+#ifdef PID
+  open(14,file=ic_name('zipid'),status='replace',access='stream')
+  if (head) print*, '  also create PID'
+#endif
+  write(12) sim ! occupying 128 bytes
+
+  nplocal=0
+  do itz=1,nnt
+  do ity=1,nnt
+  do itx=1,nnt
+    iright=0
+    rhoce=0
+    rholocal=0
+    do k=1-npb,npt+npb ! calculate coarse mesh density
+    do j=1-npb,npt+npb
+    do i=1-npb,npt+npb
+    do imove=0,nf_shake
+      kk=nft*(itz-1)+(ncell/np_nc)*(k-1)+1+imove
+      jj=nft*(ity-1)+(ncell/np_nc)*(j-1)+1+imove
+      ii=nft*(itx-1)+(ncell/np_nc)*(i-1)+1+imove
+      xq=((/i,j,k/)-1d0)/np_nc + (0.5d0+imove)/ncell ! Lagrangian position q
+      gradphi(1)=phi(ii+1,jj,kk)-phi(ii-1,jj,kk)
+      gradphi(2)=phi(ii,jj+1,kk)-phi(ii,jj-1,kk)
+      gradphi(3)=phi(ii,jj,kk+1)-phi(ii,jj,kk-1)
+      g=ceiling(xq-gradphi/(8*pi*ncell))
+      rhoce(g(1),g(2),g(3))=rhoce(g(1),g(2),g(3))+1
+    enddo
+    enddo
+    enddo
+    enddo
+
+    cume=cumsum3(rhoce)
+
+    do k=1-npb,npt+npb ! create particles in extended mesh
+    do j=1-npb,npt+npb
+    do i=1-npb,npt+npb
+    do imove=0,nf_shake
+      kk=nft*(itz-1)+(ncell/np_nc)*(k-1)+1+imove
+      jj=nft*(ity-1)+(ncell/np_nc)*(j-1)+1+imove
+      ii=nft*(itx-1)+(ncell/np_nc)*(i-1)+1+imove
+      xq=((/i,j,k/)-1d0)/np_nc + (0.5d0+imove)/ncell
+      gradphi(1)=phi(ii+1,jj,kk)-phi(ii-1,jj,kk)
+      gradphi(2)=phi(ii,jj+1,kk)-phi(ii,jj-1,kk)
+      gradphi(3)=phi(ii,jj,kk+1)-phi(ii,jj,kk-1)
+      g=ceiling(xq-gradphi/(8*pi*ncell))
+      rholocal(g(1),g(2),g(3))=rholocal(g(1),g(2),g(3))+1
+      idx=cume(g(1),g(2),g(3))-rhoce(g(1),g(2),g(3))+rholocal(g(1),g(2),g(3))
+      x(:,idx)=floor((xq-gradphi/(8*pi*ncell))/x_resolution)
+      v(:,idx)=nint(-gradphi/(8*pi)*vf/v_i2r,kind=izipv)
+#ifdef PID
+      pid(1,idx)=this_image()-1
+      pid(2:4,idx)=floor(( ((/itx,ity,itz/)-1)*nft+(ncell/np_nc)*((/i,j,k/)-1)+0.5+imove )/nf*2**(8*izipx)-2**(8*izipx-1))
+#endif
+      !if (itx==1 .and. ity==1 .and. itz==1 .and. i==1 .and. j==1 .and. k==1) then
+      !  print*, 'idx',idx
+      !  !print*, x(:,idx)
+      !  print*, (x(:,idx)+ishift+rshift)*x_resolution*ncell + (g-1)*ncell
+      !  print*, v(:,idx)*v_i2r
+      !endif
+      !if (itx==nnt .and. ity==nnt .and. itz==nnt .and. i==npt .and. j==npt .and. k==npt) then
+      !  print*, 'idx',idx
+      !  !print*, x(:,idx)
+      !  print*, (x(:,idx)+ishift+rshift)*x_resolution*ncell + (g-1)*ncell
+      !  print*, v(:,idx)*v_i2r
+      !endif
+    enddo
+    enddo
+    enddo
+    enddo
+
+    do k=1,nt ! delete buffer particles
+    do j=1,nt
+      ileft=iright+1
+      nlast=cume(nt,j,k)
+      nlen=nlast-cume(0,j,k)
+      iright=ileft+nlen-1
+      x(:,ileft:iright)=x(:,nlast-nlen+1:nlast)
+      v(:,ileft:iright)=v(:,nlast-nlen+1:nlast)
+#ifdef PID
+      pid(:,ileft:iright)=pid(:,nlast-nlen+1:nlast)
+#endif
+    enddo
+    enddo
+
+    write(10) x(:,1:iright)
+    write(11) v(:,1:iright)
+#ifdef PID
+    write(14) pid(:,1:iright)
+#endif
+    write(12) rhoce(1:nt,1:nt,1:nt)
+
+    nplocal=nplocal+iright
+
+  enddo
+  enddo
+  enddo
+  
+  close(10)
+  close(11)
+#ifdef PID
+  close(14)
+#endif
+
+  sim%nplocal=nplocal
+  sim%v_r2i=1/v_i2r
+  rewind(12)
+  write(12) sim
+  close(12)
+
+  print*,'image',this_image(),', nplocal',nplocal
+  sync all
+  npglobal=0
+  do i=1,nn**3
+    npglobal=npglobal+nplocal[i]
+  enddo
+  if (head) print*, 'npglobal =',npglobal
+  sim%mass_p=real(nf_global**3,kind=8)/npglobal
+  call print_header(sim)
+  
+  if (head) print*, 'initial condition done'
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  contains
+
+  function cumsum3(input)
+  implicit none
+  integer input(1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb)
+  integer cumsum3(1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb)
+  integer nsum,igx,igy,igz
+  nsum=0
+  do igz=1-2*ncb,nt+2*ncb
+  do igy=1-2*ncb,nt+2*ncb
+  do igx=1-2*ncb,nt+2*ncb
+    nsum=nsum+input(igx,igy,igz)
+    cumsum3(igx,igy,igz)=nsum
+  enddo
+  enddo
+  enddo
+  endfunction cumsum3
+
+  function interp_tf(kr,ix,iy)
+  implicit none
+  integer ix,iy,ii,i1,i2
+  real kr,xx,yy,x1,x2,y1,y2,interp_tf
+  i1=1
+  i2=nk
+  do while (i2-i1>1)
+    ii=(i1+i2)/2
+    if (kr>tf(ix,ii)) then
+      i1=ii
+    else
+      i2=ii
+    endif
+  enddo
+  x1=log(tf(ix,i1))
+  y1=log(tf(iy,i1))
+  x2=log(tf(ix,i2))
+  y2=log(tf(iy,i2))
+  xx=log(kr)
+  yy=y1+(y2-y1)*(xx-x1)/(x2-x1)
+  interp_tf=exp(yy)
+  endfunction interp_tf
+
+
+  function tophat(x)
+    implicit none
+    real :: x,tophat
+    if (x/=0) then
+      tophat=3*(sin(x)-cos(x)*x)/x**3
+    else
+      tophat=1
+    endif
+  endfunction tophat
+
+  function Dgrow(a)
+    implicit none
+    real, parameter :: om=omega_m
+    real, parameter :: ol=omega_l
+    real a
+    real Dgrow
+    real g,ga,hsq,oma,ola
+    hsq=om/a**3+(1-om-ol)/a**2+ol
+    oma=om/(a**3*hsq)
+    ola=ol/hsq
+    g=2.5*om/(om**(4./7)-ol+(1+om/2)*(1+ol/70))
+    ga=2.5*oma/(oma**(4./7)-ola+(1+oma/2)*(1+ola/70))
+    Dgrow=a*ga/g
+  end function Dgrow
+
+  function vfactor(a)
+    implicit none
+    real :: a
+    real :: H,km,lm
+    real :: vfactor
+    lm=omega_l/omega_m
+    km=(1-omega_m-omega_l)/omega_m
+    H=2/(3*sqrt(a**3))*sqrt(1+a*km+a**3*lm)
+    vfactor=a**2*H
+  endfunction vfactor
+
+  function lcg(s) !// Linear congruential generator
+    implicit none
+    integer :: lcg
+    integer(int64) :: s
+    if (s == 0) then
+       s = 104729
+    else
+       s = mod(s, 4294967296_int64)
+    end if
+    s = mod(s * 279470273_int64, 4294967291_int64)
+    lcg = int(mod(s, int(huge(0), int64)), kind(0))
+  endfunction
+
+#ifdef DO_2LPT
+  subroutine correct_2lpt
     ! phi_xx
     do k=1,npen
     do j=1,nf
@@ -556,24 +839,7 @@ program initial_conditions
     open(11,file='phi12.dat',status='replace',access='stream')
     write(11) phi(1:nf,1:nf,1:nf)
     close(11)
-#endif
 
-
-  !#ifdef ZA
-  !  phi(1:nf,1:nf,1:nf)=phixx
-  !#endif
-
-  ! buffer phi
-
-  phi(:0,:,:)=phi(nf-nfb:nf,:,:)[image1d(inx,icy,icz)]
-  phi(nf+1:,:,:)=phi(1:nfb+1,:,:)[image1d(ipx,icy,icz)]
-  phi(:,:0,:)=phi(:,nf-nfb:nf,:)[image1d(icx,iny,icz)]
-  phi(:,nf+1:,:)=phi(:,1:nfb+1,:)[image1d(icx,ipy,icz)]
-  phi(:,:,:0)=phi(:,:,nf-nfb:nf)[image1d(icx,icy,inz)]
-  phi(:,:,nf+1:)=phi(:,:,1:nfb+1)[image1d(icx,icy,ipz)]
-
-#ifndef ZA
-    ! buffer phi1
     phi1(1:nf,1:nf,1:nf)=phixx
     phi1(:0,:,:)=phi1(nf-nfb:nf,:,:)[image1d(inx,icy,icz)]
     phi1(nf+1:,:,:)=phi1(1:nfb+1,:,:)[image1d(ipx,icy,icz)]
@@ -581,242 +847,7 @@ program initial_conditions
     phi1(:,nf+1:,:)=phi1(:,1:nfb+1,:)[image1d(icx,ipy,icz)]
     phi1(:,:,:0)=phi1(:,:,nf-nfb:nf)[image1d(icx,icy,inz)]
     phi1(:,:,nf+1:)=phi1(:,:,1:nfb+1)[image1d(icx,icy,ipz)]
+  endsubroutine
 #endif
-  call destroy_penfft_plan
-
-  vf=vfactor(a)
-  print*, 'vf',vf
-  grad_max(1)=maxval(abs(phi(0:nf-1,1:nf,1:nf)-phi(2:nf+1,1:nf,1:nf)))
-  grad_max(2)=maxval(abs(phi(1:nf,0:nf-1,1:nf)-phi(1:nf,2:nf+1,1:nf)))
-  grad_max(3)=maxval(abs(phi(1:nf,1:nf,0:nf-1)-phi(1:nf,1:nf,2:nf+1)))
-
-  vmax=grad_max/2/(4*pi)*vf
-
-  v_i2r=vmax*v_resolution
-
-  print*, 'grad_max',grad_max
-  print*, 'vmax',vmax
-  ! create particles (no communication)
-
-  print*, 'phi', phi(1:4,1,1)
-
-  open(10,file=ic_name('zip0'),status='replace',access='stream')
-  open(11,file=ic_name('zip1'),status='replace',access='stream')
-  open(12,file=ic_name('zip2'),status='replace',access='stream')
-#ifdef PID
-  open(14,file=ic_name('zipid'),status='replace',access='stream')
-#endif
-
-  write(12) sim ! occupying 128 bytes
-
-  nplocal=0
-  do itz=1,nnt
-  do ity=1,nnt
-  do itx=1,nnt
-    iright=0
-    rhoce=0
-    rholocal=0
-    do k=1-npb,npt+npb ! calculate coarse mesh density
-    do j=1-npb,npt+npb
-    do i=1-npb,npt+npb
-    do imove=0,nf_shake
-      kk=nft*(itz-1)+(ncell/np_nc)*(k-1)+1+imove
-      jj=nft*(ity-1)+(ncell/np_nc)*(j-1)+1+imove
-      ii=nft*(itx-1)+(ncell/np_nc)*(i-1)+1+imove
-      g(1)=ceiling( real(i-1)/np_nc+(0.5+imove)/ncell + (phi(ii-1,jj,kk)-phi(ii+1,jj,kk))/2/(4*pi)/ncell )
-      g(2)=ceiling( real(j-1)/np_nc+(0.5+imove)/ncell + (phi(ii,jj-1,kk)-phi(ii,jj+1,kk))/2/(4*pi)/ncell )
-      g(3)=ceiling( real(k-1)/np_nc+(0.5+imove)/ncell + (phi(ii,jj,kk-1)-phi(ii,jj,kk+1))/2/(4*pi)/ncell )
-      rhoce(g(1),g(2),g(3))=rhoce(g(1),g(2),g(3))+1
-      !if (i==1 .and. j==1 .and. k==1) then
-      !  print*, 4*( real(i-1)/np_nc+0.5/ncell + (phi(ii-1,jj,kk)-phi(ii+1,jj,kk))/2/(4*pi)/ncell )
-      !  print*, 4*( real(j-1)/np_nc+0.5/ncell + (phi(ii,jj-1,kk)-phi(ii,jj+1,kk))/2/(4*pi)/ncell )
-      !  print*, 4*( real(k-1)/np_nc+0.5/ncell + (phi(ii,jj,kk-1)-phi(ii,jj,kk+1))/2/(4*pi)/ncell )
-      !endif
-    enddo
-    enddo
-    enddo
-    enddo
-
-    cume=cumsum3(rhoce)
-
-    do k=1-npb,npt+npb ! create particles in extended mesh
-    do j=1-npb,npt+npb
-    do i=1-npb,npt+npb
-    do imove=0,nf_shake
-      kk=nft*(itz-1)+(ncell/np_nc)*(k-1)+1+imove
-      jj=nft*(ity-1)+(ncell/np_nc)*(j-1)+1+imove
-      ii=nft*(itx-1)+(ncell/np_nc)*(i-1)+1+imove
-      g(1)=ceiling( real(i-1)/np_nc+(0.5+imove)/ncell + (phi(ii-1,jj,kk)-phi(ii+1,jj,kk))/2/(4*pi)/ncell )
-      g(2)=ceiling( real(j-1)/np_nc+(0.5+imove)/ncell + (phi(ii,jj-1,kk)-phi(ii,jj+1,kk))/2/(4*pi)/ncell )
-      g(3)=ceiling( real(k-1)/np_nc+(0.5+imove)/ncell + (phi(ii,jj,kk-1)-phi(ii,jj,kk+1))/2/(4*pi)/ncell )
-      rholocal(g(1),g(2),g(3))=rholocal(g(1),g(2),g(3))+1
-      idx=cume(g(1),g(2),g(3))-rhoce(g(1),g(2),g(3))+rholocal(g(1),g(2),g(3))
-      x(1,idx)=floor(( real(i-1)/np_nc+(0.5+imove)/ncell + (phi(ii-1,jj,kk)-phi(ii+1,jj,kk))/2/(4*pi)/ncell )/x_resolution)
-      x(2,idx)=floor(( real(j-1)/np_nc+(0.5+imove)/ncell + (phi(ii,jj-1,kk)-phi(ii,jj+1,kk))/2/(4*pi)/ncell )/x_resolution)
-      x(3,idx)=floor(( real(k-1)/np_nc+(0.5+imove)/ncell + (phi(ii,jj,kk-1)-phi(ii,jj,kk+1))/2/(4*pi)/ncell )/x_resolution)
-      v(1,idx)=nint( (phi(ii-1,jj,kk)-phi(ii+1,jj,kk))/2/(4*pi)*vf/v_i2r(1), kind=izipv)
-      v(2,idx)=nint( (phi(ii,jj-1,kk)-phi(ii,jj+1,kk))/2/(4*pi)*vf/v_i2r(2), kind=izipv)
-      v(3,idx)=nint( (phi(ii,jj,kk-1)-phi(ii,jj,kk+1))/2/(4*pi)*vf/v_i2r(3), kind=izipv)
-#ifdef PID
-      pid(1,idx)=this_image()-1
-      pid(2:4,idx)=floor(( ((/itx,ity,itz/)-1)*nft+(ncell/np_nc)*((/i,j,k/)-1)+0.5+imove )/nf*2**16-2**15)
-#endif
-      !if (itx==1 .and. ity==1 .and. itz==1 .and. i==1 .and. j==1 .and. k==1) then
-      !  print*, 'idx',idx
-      !  !print*, x(:,idx)
-      !  print*, (x(:,idx)+ishift+rshift)*x_resolution*ncell + (g-1)*ncell
-      !  print*, v(:,idx)*v_i2r
-      !endif
-      !if (itx==nnt .and. ity==nnt .and. itz==nnt .and. i==npt .and. j==npt .and. k==npt) then
-      !  print*, 'idx',idx
-      !  !print*, x(:,idx)
-      !  print*, (x(:,idx)+ishift+rshift)*x_resolution*ncell + (g-1)*ncell
-      !  print*, v(:,idx)*v_i2r
-      !endif
-    enddo
-    enddo
-    enddo
-    enddo
-
-    do k=1,nt ! compact particle
-    do j=1,nt
-      ileft=iright+1
-      nlast=cume(nt,j,k)
-      nlen=nlast-cume(0,j,k)
-      iright=ileft+nlen-1
-      x(:,ileft:iright)=x(:,nlast-nlen+1:nlast)
-      v(:,ileft:iright)=v(:,nlast-nlen+1:nlast)
-#ifdef PID
-      pid(:,ileft:iright)=pid(:,nlast-nlen+1:nlast)
-#endif
-    enddo
-    enddo
-
-    write(10) x(:,1:iright)
-    write(11) v(:,1:iright)
-#ifdef PID
-    write(14) pid(:,1:iright)
-#endif
-    write(12) rhoce(1:nt,1:nt,1:nt)
-
-    nplocal=nplocal+iright
-
-  enddo
-  enddo
-  enddo
-
-  close(10)
-  close(11)
-#ifdef PID
-  close(14)
-#endif
-
-
-
-  sim%nplocal=nplocal
-  sim%mass_p=real((nf)**3)/nplocal
-  sim%v_r2i=1/v_i2r
-  rewind(12)
-  write(12) sim
-  close(12)
-
-  call print_header(sim)
-
-  if (head) print*, 'initial condition done'
-
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-  contains
-
-  function cumsum3(input)
-  implicit none
-  integer input(1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb)
-  integer cumsum3(1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb)
-  integer nsum,igx,igy,igz
-  nsum=0
-  do igz=1-2*ncb,nt+2*ncb
-  do igy=1-2*ncb,nt+2*ncb
-  do igx=1-2*ncb,nt+2*ncb
-    nsum=nsum+input(igx,igy,igz)
-    cumsum3(igx,igy,igz)=nsum
-  enddo
-  enddo
-  enddo
-  endfunction cumsum3
-
-  function interp_tf(kr,ix,iy)
-  implicit none
-  integer ix,iy,ii,i1,i2
-  real kr,xx,yy,x1,x2,y1,y2,interp_tf
-  i1=1
-  i2=nk
-  do while (i2-i1>1)
-    ii=(i1+i2)/2
-    if (kr>tf(ix,ii)) then
-      i1=ii
-    else
-      i2=ii
-    endif
-  enddo
-  x1=log(tf(ix,i1))
-  y1=log(tf(iy,i1))
-  x2=log(tf(ix,i2))
-  y2=log(tf(iy,i2))
-  xx=log(kr)
-  yy=y1+(y2-y1)*(xx-x1)/(x2-x1)
-  interp_tf=exp(yy)
-  endfunction interp_tf
-
-
-  function tophat(x)
-    implicit none
-    real :: x,tophat
-    if (x/=0) then
-      tophat=3*(sin(x)-cos(x)*x)/x**3
-    else
-      tophat=1
-    endif
-  endfunction tophat
-
-  function Dgrow(a)
-    implicit none
-    real, parameter :: om=omega_m
-    real, parameter :: ol=omega_l
-    real a
-    real Dgrow
-    real g,ga,hsq,oma,ola
-    hsq=om/a**3+(1-om-ol)/a**2+ol
-    oma=om/(a**3*hsq)
-    ola=ol/hsq
-    g=2.5*om/(om**(4./7)-ol+(1+om/2)*(1+ol/70))
-    ga=2.5*oma/(oma**(4./7)-ola+(1+oma/2)*(1+ola/70))
-    Dgrow=a*ga/g
-  end function Dgrow
-
-  function vfactor(a)
-    implicit none
-    real :: a
-    real :: H,km,lm
-    real :: vfactor
-    lm=omega_l/omega_m
-    km=(1-omega_m-omega_l)/omega_m
-    H=2/(3*sqrt(a**3))*sqrt(1+a*km+a**3*lm)
-    vfactor=a**2*H
-  endfunction vfactor
-
-  function lcg(s) !// Linear congruential generator
-    implicit none
-    integer :: lcg
-    integer(int64) :: s
-    if (s == 0) then
-       s = 104729
-    else
-       s = mod(s, 4294967296_int64)
-    end if
-    s = mod(s * 279470273_int64, 4294967291_int64)
-    lcg = int(mod(s, int(huge(0), int64)), kind(0))
-  endfunction
 
 end
