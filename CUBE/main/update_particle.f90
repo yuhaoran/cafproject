09f7d2e250dc57818044fa9f01c7961fae97add4
yuhaoran <haoran@cita.utoronto.ca>
2017-04-20 23:39:05 -0400

    print more info for timing

--- CUBE/main/update_particle.f90
+++ CUBE/main/update_particle.f90
@@ -1,221 +1,183 @@
-!#define randomv
 subroutine update_particle
-use variables
-implicit none
-save
+  use variables
+  implicit none
+  save
 
-! x(1:3,:)
-integer(8) ileft,iright,nlast,nlen,idx
-integer(8) g(3),np ! index of grid
-integer(4) rhoce(1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb) ! double buffer tile
-integer(8) cume(1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb)
-integer(4) rholocal(1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb) ! count writing
-real vrand(3), v_th
-integer(8) ii,jj,kk
-integer(8) irand(3)
-integer(8) iii(3)
-integer(8) pr1,pr2,pr3,pr4
+  ! x(1:3,:)
+  integer(8) ileft,iright,nlast,nlen,idx
+  integer(8) g(3),np ! index of grid
+  integer(4) rhoce(1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb) ! double buffer tile
+  integer(8) cume(1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb)
+  integer(4) rholocal(1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb,1-2*ncb:nt+2*ncb) ! count writing
+  integer(8) ii,jj,kk
+  integer(8) iii(3)
+  integer(8) pr1,pr2,pr3,pr4
 
-if (head) print*,'update_particle'
-
-#ifdef randomv
-  v_th=1.5/real(int(2,8)**(izipx*8))*ncell*0.5
-
-  pr1=71
-  pr2=73
-  pr3=79
-  pr4=83
-  iii=(/1,3,7/)
-#endif
-dt_mid=(dt_old+dt)/2
+  if (head) print*,'update_particle'
+  dt_mid=(dt_old+dt)/2
 
 
-overhead_tile=0
-iright=0
-do itz=1,nnt ! loop over tile
-do ity=1,nnt
-do itx=1,nnt
-  rhoce=0
-  rholocal=0
-  x_new=0
-  v_new=0
-# ifdef PID
-  pid_new=0
-# endif
+  overhead_tile=0
+  iright=0
+  do itz=1,nnt ! loop over tile
+  do ity=1,nnt
+  do itx=1,nnt
+  if (head) print*,'  tile',int(itx,1),int(ity,1),int(itz,1)
+    rhoce=0
+    rholocal=0
+    x_new=0
+    v_new=0
+#   ifdef PID
+    pid_new=0
+#   endif
 
-  do k=1-ncb,nt+ncb ! loop over coarse grid
-  do j=1-ncb,nt+ncb
-  do i=1-ncb,nt+ncb
-    nlast=cum(i,j,k,itx,ity,itz)
-    np=rhoc(i,j,k,itx,ity,itz)
-    do l=1,np
-      ip=nlast-np+l
-#ifdef randomv
-      ii=i+(itx-1)*nt
-      jj=j+(ity-1)*nt
-      kk=k+(itz-1)*nt
-      irand=(abs(ii-jj)+1)*(abs(jj-kk)+1)*ii*jj*kk*(nc-ii+1)*(nc-jj+1)*(nc-kk+1)+iii+its+l
-      irand=mod(irand,pr1)
-      irand=mod(irand**2+ii*pr1,pr2)
-      irand=mod(irand**2+jj*pr2,pr3)
-      irand=mod(irand**2+kk*pr3,pr4)
-      vrand=(irand+0.5)/pr4-0.5
-      vreal=tan(pi*real(v(:,ip))/real(nvbin-1))/(sqrt(pi/2)/sigma_vi_old)
-      vrand=vrand*(1/(1+int(abs(dt_mid*vreal/v_th))))
-#else
-      vrand=0
-#endif
-      xq=(/i,j,k/)-1d0 + (int(x(:,ip)+ishift,izipx)+rshift)*x_resolution
-      vreal=tan(pi*real(v(:,ip))/real(nvbin-1))/(sqrt(pi/2)/sigma_vi_old)
-      deltax=dt_mid*vreal/4+(x_resolution*ncell)*vrand
-      g=ceiling(xq+deltax)
-      rhoce(g(1),g(2),g(3))=rhoce(g(1),g(2),g(3))+1 ! update mesh
-!print*,x(:,ip)
-!print*,xq,deltax,g;stop
+    if (head) print*,'    density loop'
+    do k=1-ncb,nt+ncb ! loop over coarse grid
+    do j=1-ncb,nt+ncb
+    do i=1-ncb,nt+ncb
+      nlast=cum(i,j,k,itx,ity,itz)
+      np=rhoc(i,j,k,itx,ity,itz)
+      do l=1,np
+        ip=nlast-np+l
+
+        xq=(/i,j,k/)-1d0 + (int(x(:,ip)+ishift,izipx)+rshift)*x_resolution
+        vreal=tan(pi*real(v(:,ip))/real(nvbin-1))/(sqrt(pi/2)/sigma_vi_old)
+        deltax=dt_mid*vreal/4
+        g=ceiling(xq+deltax)
+        rhoce(g(1),g(2),g(3))=rhoce(g(1),g(2),g(3))+1 ! update mesh
+        !print*,x(:,ip)
+        !print*,xq,deltax,g;stop
+      enddo
     enddo
+    enddo
+    enddo
+
+    if (head) print*,'    cubesum3'
+    cume=cumsum3(rhoce)
+    overhead_tile=max(overhead_tile,cume(nt+2*ncb,nt+2*ncb,nt+2*ncb)/real(np_tile_max))
+
+    if (cume(nt+2*ncb,nt+2*ncb,nt+2*ncb)>np_tile_max) then
+      print*, '  error: too many particles in this tile+buffer'
+      print*, '  ',cume(nt+2*ncb,nt+2*ncb,nt+2*ncb),'>',np_tile_max
+      print*, '  on',this_image(), itx,ity,itz
+      print*, '  please set tile_buffer larger'
+      stop
+    endif
+
+    ! create a new x and v for this local tile
+
+  if (head) print*,'    xvnew loop'
+    do k=1-ncb,nt+ncb ! update particle
+    do j=1-ncb,nt+ncb
+    do i=1-ncb,nt+ncb
+      nlast=cum(i,j,k,itx,ity,itz)
+      np=rhoc(i,j,k,itx,ity,itz)
+      do l=1,np
+        ip=nlast-np+l
+        xq=(/i,j,k/)-1d0 + (int(x(:,ip)+ishift,izipx)+rshift)*x_resolution
+        vreal=tan(pi*real(v(:,ip))/real(nvbin-1))/(sqrt(pi/2)/sigma_vi_old)
+        deltax=dt_mid*vreal/4
+        g=ceiling(xq+deltax)
+        rholocal(g(1),g(2),g(3))=rholocal(g(1),g(2),g(3))+1
+        idx=cume(g(1),g(2),g(3))-rhoce(g(1),g(2),g(3))+rholocal(g(1),g(2),g(3)) ! index for writing
+
+#       ifdef debug
+          x_new(:,idx)=x_new(:,idx)+1
+#       else
+        x_new(:,idx)=x(:,ip)+nint(dt_mid*vreal/(x_resolution*ncell))
+  !print*, nlast,np,l,ip
+  !print*, x(:,ip)
+  !print*, idx
+  !print*, x_new(:,idx)
+  !stop
+#endif
+        v_new(:,idx)=v(:,ip)
+  !print*, v(:,ip)
+  !print*, v_new(:,idx)
+  !stop
+#ifdef PID
+        pid_new(:,idx)=pid(:,ip)
+#endif
+        ! update mesh
+        ! rhoce_new
+      enddo
+    enddo
+    enddo
+    enddo
+
+#ifdef debug
+    print*, sum(abs(x_new(1,1:nptile_old)-1)*1)
+#endif
+    ! delete particles
+
+    sync all
+
+  if (head) print*,'    delete_particle loop'
+    do k=1,nt
+    do j=1,nt
+      ileft=iright+1
+      nlast=cume(nt,j,k)
+      nlen=nlast-cume(0,j,k)
+      iright=ileft+nlen-1
+      x(:,ileft:iright)=x_new(:,nlast-nlen+1:nlast)
+      v(:,ileft:iright)=v_new(:,nlast-nlen+1:nlast)
+  !print*,ileft,iright,nlast,nlen
+  !print*, x(:,ileft:iright)
+  !print*, x_new(:,nlast-nlen+1:nlast)
+  !stop
+
+#ifdef PID
+      pid(:,ileft:iright)=pid_new(:,nlast-nlen+1:nlast)
+#endif
+    enddo
+    enddo
+
+    ! update rhoc
+    rhoc(1:nt,1:nt,1:nt,itx,ity,itz)=rhoce(1:nt,1:nt,1:nt)
+    !print*, sum(rhoce(1:nt,1:nt,1:nt)), sum(rhoce), cume(nt+2*ncb,nt+2*ncb,nt+2*ncb), sum(rhoc(1:nt,1:nt,1:nt,itx,ity,itz))
   enddo
   enddo
   enddo
 
-  cume=cumsum3(rhoce)
-  overhead_tile=max(overhead_tile,cume(nt+2*ncb,nt+2*ncb,nt+2*ncb)/real(np_tile_max))
+  nplocal=iright
+  print*,x(:,1),v(:,1)
+  do i=1,nn**3
+    overhead_tile=max(overhead_tile,overhead_tile[i])
+  enddo
 
-  if (cume(nt+2*ncb,nt+2*ncb,nt+2*ncb)>np_tile_max) then
-    print*, '  error: too many particles in this tile+buffer'
-    print*, '  ',cume(nt+2*ncb,nt+2*ncb,nt+2*ncb),'>',np_tile_max
-    print*, '  on',this_image(), itx,ity,itz
-    print*, '  please set tile_buffer larger'
-    stop
+  if (head) then
+    print*, '  tile overhead',overhead_tile*100,'% full'
+    print*, '  comsumed tile_buffer =',overhead_tile*tile_buffer,'/',tile_buffer
+    print*, '  clean buffer of rhoc'
   endif
 
-  ! create a new x and v for this local tile
+  ! clean up buffer region of rhoc
 
-  do k=1-ncb,nt+ncb ! update particle
-  do j=1-ncb,nt+ncb
-  do i=1-ncb,nt+ncb
-    nlast=cum(i,j,k,itx,ity,itz)
-    np=rhoc(i,j,k,itx,ity,itz)
-    do l=1,np
-      ip=nlast-np+l
-#ifdef randomv
-      ii=i+(itx-1)*nt
-      jj=j+(ity-1)*nt
-      kk=k+(itz-1)*nt
-      irand=(abs(ii-jj)+1)*(abs(jj-kk)+1)*ii*jj*kk*(nc-ii+1)*(nc-jj+1)*(nc-kk+1)+iii+its+l
-      irand=mod(irand,pr1)
-      irand=mod(irand**2+ii*pr1,pr2)
-      irand=mod(irand**2+jj*pr2,pr3)
-      irand=mod(irand**2+kk*pr3,pr4)
-      vrand=(irand+0.5)/pr4-0.5
-      vreal=tan(pi*real(v(:,ip))/real(nvbin-1))/(sqrt(pi/2)/sigma_vi_old)
-      vrand=vrand*(1/(1+int(abs(dt_mid*vreal/v_th))))
-#else
-      vrand=0
-#endif
-      xq=(/i,j,k/)-1d0 + (int(x(:,ip)+ishift,izipx)+rshift)*x_resolution
-      vreal=tan(pi*real(v(:,ip))/real(nvbin-1))/(sqrt(pi/2)/sigma_vi_old)
-      deltax=dt_mid*vreal/4+(x_resolution*ncell)*vrand
-      g=ceiling(xq+deltax)
-      rholocal(g(1),g(2),g(3))=rholocal(g(1),g(2),g(3))+1
-      idx=cume(g(1),g(2),g(3))-rhoce(g(1),g(2),g(3))+rholocal(g(1),g(2),g(3)) ! index for writing
+  rhoc(:0,:,:,:,:,:)=0
+  rhoc(nt+1:,:,:,:,:,:)=0
+  rhoc(:,:0,:,:,:,:)=0
+  rhoc(:,nt+1:,:,:,:,:)=0
+  rhoc(:,:,:0,:,:,:)=0
+  rhoc(:,:,nt+1:,:,:,:)=0
 
-#ifdef debug
-      x_new(:,idx)=x_new(:,idx)+1
-#else
-      x_new(:,idx)=x(:,ip)+nint(dt_mid*vreal/(x_resolution*ncell) + vrand)
-!print*, nlast,np,l,ip
-!print*, x(:,ip)
-!print*, nint(dt_mid*vreal/(x_resolution*ncell) + vrand)
-!print*, idx
-!print*, x_new(:,idx)
-!stop
-#endif
-      v_new(:,idx)=v(:,ip)
-!print*, v(:,ip)
-!print*, v_new(:,idx)
-!stop
+  ! clean up x, v beyond nplocal
+  !x(:,nplocal+1:)=0
+  !v(:,nplocal+1:)=0
 #ifdef PID
-      pid_new(:,idx)=pid(:,ip)
+  !pid(:,nplocal+1:)=0
 #endif
-      ! update mesh
-      ! rhoce_new
-    enddo
-  enddo
-  enddo
-  enddo
-
-#ifdef debug
-  print*, sum(abs(x_new(1,1:nptile_old)-1)*1)
-#endif
-  ! delete particles
+  print*,'  check xv of first particle',x(:,1),v(:,1)
 
   sync all
 
-  do k=1,nt
-  do j=1,nt
-    ileft=iright+1
-    nlast=cume(nt,j,k)
-    nlen=nlast-cume(0,j,k)
-    iright=ileft+nlen-1
-    x(:,ileft:iright)=x_new(:,nlast-nlen+1:nlast)
-    v(:,ileft:iright)=v_new(:,nlast-nlen+1:nlast)
-!print*,ileft,iright,nlast,nlen
-!print*, x(:,ileft:iright)
-!print*, x_new(:,nlast-nlen+1:nlast)
-!stop
+  if (head) then
+    npcheck=0
+    do i=1,nn**3
+      npcheck=npcheck+nplocal[i]
+    enddo
+    print*, '  npcheck,npglobal=', npcheck,npglobal
+  endif
 
-#ifdef PID
-    pid(:,ileft:iright)=pid_new(:,nlast-nlen+1:nlast)
-#endif
-  enddo
-  enddo
-
-  ! update rhoc
-  rhoc(1:nt,1:nt,1:nt,itx,ity,itz)=rhoce(1:nt,1:nt,1:nt)
-  !print*, sum(rhoce(1:nt,1:nt,1:nt)), sum(rhoce), cume(nt+2*ncb,nt+2*ncb,nt+2*ncb), sum(rhoc(1:nt,1:nt,1:nt,itx,ity,itz))
-enddo
-enddo
-enddo
-
-nplocal=iright
-print*,x(:,1),v(:,1)
-do i=1,nn**3
-  overhead_tile=max(overhead_tile,overhead_tile[i])
-enddo
-
-if (head) then
-  print*, '  tile overhead',overhead_tile*100,'% full'
-  print*, '  comsumed tile_buffer =',overhead_tile*tile_buffer,'/',tile_buffer
-  print*, '  clean buffer of rhoc'
-endif
-
-! clean up buffer region of rhoc
-
-rhoc(:0,:,:,:,:,:)=0
-rhoc(nt+1:,:,:,:,:,:)=0
-rhoc(:,:0,:,:,:,:)=0
-rhoc(:,nt+1:,:,:,:,:)=0
-rhoc(:,:,:0,:,:,:)=0
-rhoc(:,:,nt+1:,:,:,:)=0
-
-! clean up x, v beyond nplocal
-!x(:,nplocal+1:)=0
-!v(:,nplocal+1:)=0
-#ifdef PID
-!pid(:,nplocal+1:)=0
-#endif
-print*,x(:,1),v(:,1)
-
-sync all
-
-if (head) then
-  npcheck=0
-  do i=1,nn**3
-    npcheck=npcheck+nplocal[i]
-  enddo
-  print*, '  npcheck,npglobal=', npcheck,npglobal
-endif
-
-sync all
+  sync all
 
 endsubroutine update_particle
