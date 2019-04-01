d53a36cd9c20808b4ba7c38fec725bd668bca568
Haoran Yu <haoran@cita.utoronto.ca>
2019-04-01 14:44:20 -0400

    two point correlation function for discrete and continuous vector field

--- CUBEnu/work/utilities/lpt.f90
+++ CUBEnu/work/utilities/lpt.f90
@@ -1,6 +1,6 @@
 !#define Emode
 !#define specify_potential
-#define get_tcpf
+#define debug
 program lpt
 !  use omp_lib
   use variables, only: spine_tile
@@ -15,25 +15,20 @@ program lpt
   real kr,kx,ky,kz,pow,r_filter,rm,masstemp
   real spin_u(3),spin_v(3),spin_r(3),spin_e(3,3),spin_vor(3)
 
-
   real phi(0:nf+1,0:nf+1,0:nf+1)[*]
   real phi_large(0:nf+1,0:nf+1,0:nf+1)[*]
   complex phi_k(nyquest+1,nf,npen)
   real delta2(ng,ng,ng)
-  integer i,j,k,n_rsmall,n_ratio,nmassbin,itemp
+  integer i,j,k,n_rsmall,n_ratio,nmassbin,itemp,l
   real t11,t22,t33,t12,t23,t31
   real tsmall(3,3),tlarge(3,3),torque(3,3)
-  real spin(3,ng,ng,ng),spinmag(ng,ng,ng)
+  real spin(3,ng,ng,ng),spinmag(ng,ng,ng),n_object(ng,ng,ng)
   type(type_halo_info) halo_info
   type(type_halo_catalog) halo_catalog
   real,allocatable :: spin_x(:,:),spin_q(:,:),spin_t(:,:),theta(:,:),imass(:),imass_info(:,:)
   real,allocatable :: corr_x(:,:,:),corr_q(:,:,:),corr_t(:,:,:),r_small(:),ratio_scale(:)
   integer,allocatable :: ind(:,:),isort_mass(:),i1(:),i2(:),ii1,ii2
-#ifdef get_tcpf
-  integer i_dim
-  complex cmu(ng*nn/2+1,ng,npen)
-#endif
-
+  equivalence(spinmag,n_object)
 
 #ifdef specify_potential
   !character(*),parameter :: fn='/mnt/raid-cita/haoran/spin/cafproject/CUBEnu/output/universe38/image1/50.000_phi1_1.bin'
@@ -43,7 +38,7 @@ program lpt
   ! nc: coarse grid per image per dim
   ! nf: fine grid per image per dim, ng=nf
   ! nyquest: Nyquest frequency
-  call omp_set_num_threads(ncore)
+  !call omp_set_num_threads(ncore)
   call geometry
   open(16,file='../main/z_checkpoint.txt',status='old')
   do i=1,nmax_redshift
@@ -55,7 +50,7 @@ program lpt
   if (head) then
     print*, ''
     print*, 'program LPT'
-    print*, 'on',nn**3,' images and',ncore,' cores'
+    !print*, 'on',nn**3,' images and',ncore,' cores'
     print*, 'Resolution', ng*nn
     print*, 'at redshift=',z_checkpoint(cur_checkpoint)
     print*, 'Box size', box
@@ -99,7 +94,7 @@ program lpt
   ind=ind(:,isort_mass)
   ! construct halo mass bins
   rm=2.0 ! mass bin ratio
-  n_rsmall=50
+  n_rsmall=15
   n_ratio=20
   nmassbin=ceiling(log(imass(1)/imass(nhalo))/log(rm))
   allocate(imass_info(4,nmassbin),i1(nmassbin),i2(nmassbin))
@@ -124,10 +119,10 @@ program lpt
   enddo
   ! construct scale bins
   do i=1,n_rsmall
-    r_small(i)=0.1+0.1*(i-1)
+    r_small(i)=0.4+0.4*(i-1)
   enddo
   do i=1,n_ratio
-    ratio_scale(i)=1.1+0.3*(i-1)
+    ratio_scale(i)=1.5+0.2*(i-1)
   enddo
 
   ! open potential file
@@ -145,9 +140,13 @@ program lpt
   call pencil_fft_forward ! complex phi stored in cxyz
   phi_k=cxyz ! store raw complex phi into phi_k
 
-#ifdef get_tcpf
-  call write_spin_and_stop(1.2,1.2*1.1)
+#ifdef debug
+  call ngp_spinfield
+  call spin_analysis(0.8,2.2)
+  print*,'debug complete'
+  stop
 #endif
+
 
   cur_checkpoint=n_checkpoint
 #ifndef Emode
@@ -181,23 +180,189 @@ program lpt
 call destroy_penfft_plan
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 contains
 
-  subroutine write_spin_and_stop(r_small,ratio_scale)
-    ! use phi_k, apply 2 filters, spinfield, tpcf.
+  subroutine ngp_spinfield()
+    ! spin: discrete spin field from halos
+    ! spinmag: number count
+    implicit none
+    save
+    spin=0
+    do ihalo=1,nhalo
+      spin(:,ind(1,ihalo),ind(2,ihalo),ind(3,ihalo))=spin_q(:,ihalo)/norm2(spin_q(:,ihalo))
+      n_object(ind(1,ihalo),ind(2,ihalo),ind(3,ihalo))=1
+    enddo
+    call vtpcf(flag_discrete=.true.)
+    print*,'  called ngp_spinfield'
+  endsubroutine
+
+  subroutine spin_analysis(r_small,ratio_scale)
+    ! use phi_k, apply 2 filters, spinfield, ar.
+    implicit none
+    save
+    real r_small,ratio_scale
+    ! phi
+    call tophat_fourier_filter(phi_k,r_small)
+    call pencil_fft_backward
+    phi(1:nf,1:nf,1:nf)=r3
+    call buffer_1layer(phi)
+    ! phi+
+    call tophat_fourier_filter(phi_k,r_small*ratio_scale)
+    call pencil_fft_backward
+    phi_large(1:nf,1:nf,1:nf)=r3
+    call buffer_1layer(phi_large)
+    ! e T T+
+    call spinfield
+    !open(11,file=output_name('spinfield'),status='replace',access='stream')
+    !write(11) spin
+    !close(11)
+    call vtpcf(flag_discrete=.false.)
+    print*,'  called spin_analysis'
+  endsubroutine
+
+  subroutine vtpcf(flag_discrete)
+    ! vector-field two point correlation function
+    ! returns A(r) B(r) from angular average
     implicit none
     save
     integer,parameter :: n_dist=ng/2*sqrt(3.)+1
-    integer i_dist(3),ibin
-    real r_small,ratio_scale,r_dist,tpcf(3,n_dist)
+    logical flag_discrete
+    real proj_a,proj_b,r_dist,abr(4,n_dist),xixjr2
+    complex complex_Cij(ng*nn/2+1,ng,npen,6)
+    integer i_dim,j_dim,i_dist(3),ibin
+    complex c_i(ng*nn/2+1,ng,npen)
+
+    complex_Cij=0 ! complex L_i*L_j
+    do i_dim=1,3
+      r3=spin(i_dim,:,:,:) ! real space unit vector field, i_dim'th component
+      call pencil_fft_forward
+      c_i=cxyz
+      complex_Cij(:,:,:,i_dim) = cxyz*conjg(cxyz) ! trace
+      do j_dim=1,i_dim-1 ! off-diag
+        r3=spin(j_dim,:,:,:)
+        call pencil_fft_forward
+        complex_Cij(:,:,:,i_dim+j_dim+1) = c_i*conjg(cxyz)
+      enddo
+    enddo
+
+    if (flag_discrete) then
+      r3=n_object
+      call pencil_fft_forward
+      cxyz=real(cxyz*conjg(cxyz))
+      call pencil_fft_backward
+      n_object=r3
+    endif
+    !open(11,file=output_name('n_object'),status='replace',access='stream')
+    !write(11) r3
+    !close(11)
+    !stop
+
+    ! calculate A(r) & B(r)
+    ! diagonal part
+    !cxyz=complex_Cii/ng_global/ng_global/ng_global
+    !call pencil_fft_backward
+    !open(11,file=output_name('mu3d'),status='replace',access='stream')
+    !write(11) r3
+    !close(11)
+    ! angular average (single node)
+    !abr=0
+    !do k=1,ng
+    !do j=1,ng
+    !do i=1,ng
+    !  i_dist=mod((/i,j,k/)+ng/2-1,ng)-ng/2
+    !  r_dist=norm2(real(i_dist))
+    !  ibin=nint(r_dist)+1
+    !  abr(1,ibin)=abr(1,ibin)+1
+    !  abr(2,ibin)=abr(2,ibin)+r_dist
+    !  abr(3,ibin)=abr(3,ibin)+r3(i,j,k)
+    !enddo
+    !enddo
+    !enddo
+    !abr(2,:)=abr(2,:)/abr(1,:)*box/ng
+    !abr(3,:)=abr(3,:)/abr(1,:)
+    !open(11,file=output_name('mu1d'),status='replace',access='stream')
+    !write(11) n_dist,ar
+    !close(11)
+
+    ! off-diagonal part
+
+    ! angular average (single node)
+    abr=0
+    do l=1,6 ! xx,yy,zz,xy,xz,yz
+      if     (l==1) then
+        i_dim=1; j_dim=1; proj_a=1; proj_b=-2./3.
+      elseif (l==2) then
+        i_dim=2; j_dim=2; proj_a=1; proj_b=-2./3.
+      elseif (l==3) then
+        i_dim=3; j_dim=3; proj_a=1; proj_b=-2./3.
+      elseif (l==4) then
+        i_dim=1; j_dim=2; proj_a=0; proj_b=0
+      elseif (l==5) then
+        i_dim=1; j_dim=3; proj_a=0; proj_b=0
+      elseif (l==6) then
+        i_dim=2; j_dim=3; proj_a=0; proj_b=0
+      endif
+      cxyz=complex_Cij(:,:,:,l)
+      call pencil_fft_backward
+      if (flag_discrete) then
+        n_object=max(0.001,n_object) ! remove zeros
+        r3=r3/n_object
+      else
+        r3=r3/ng_global/ng_global/ng_global
+      endif
+      !open(11,file=output_name('nu3d'),status='replace',access='stream')
+      !write(11) r3
+      !close(11)
+      do k=1,ng
+      do j=1,ng
+      do i=1,ng
+        i_dist=mod((/i,j,k/)+ng/2-1,ng)-ng/2
+        r_dist=norm2(real(i_dist))
+        xixjr2=merge(i_dist(i_dim)*i_dist(j_dim)/r_dist**2,0.0,r_dist/=0)
+        proj_b=proj_b+2*xixjr2
+        ibin=nint(r_dist)+1
+        abr(1,ibin)=abr(1,ibin)+1
+        abr(2,ibin)=abr(2,ibin)+r_dist
+        abr(3,ibin)=abr(3,ibin)+r3(i,j,k)*proj_a
+        if (i_dim/=j_dim) abr(4,ibin)=abr(4,ibin)+r3(i,j,k)*proj_b
+      enddo
+      enddo
+      enddo
+    enddo
+    abr(2,:)=abr(2,:)/abr(1,:)*box/ng
+    abr(3,:)=abr(3,:)/abr(1,:)/1.5
+    abr(4,:)=abr(4,:)/abr(1,:)/1.5
+
+    if (flag_discrete) then
+      open(11,file=output_name('arbr_discrete'),status='replace',access='stream')
+    else
+      open(11,file=output_name('arbr'),status='replace',access='stream')
+    endif
+    write(11) n_dist,abr
+    close(11)
+    print*, '    called vtpcf'
+  endsubroutine
+
+  subroutine correlate_spin(r_small,ratio_scale)
+    implicit none
+    save
+    real r_small,ratio_scale
 
     call tophat_fourier_filter(phi_k,r_small)
     call pencil_fft_backward
-    !call tophat_fourier_filter(phi_k,6.0)
-    !call pencil_fft_backward
-    !open(11,file=output_name('phismall'),status='replace',access='stream')
-    !write(11) r3
-    !close(11)
     phi(1:nf,1:nf,1:nf)=r3
     call buffer_1layer(phi)
 
@@ -206,56 +371,6 @@ contains
     phi_large(1:nf,1:nf,1:nf)=r3
     call buffer_1layer(phi_large)
 
-    call spinfield
-    open(11,file=output_name('spinfield'),status='replace',access='stream')
-    write(11) spin
-    close(11)
-    cmu=0 ! complex mu
-    do i_dim=1,3
-      r3=spin(i_dim,:,:,:) ! real space unit vector field, i_dim'th component
-      call pencil_fft_forward
-      cmu=cmu+(abs(cxyz))**2
-    enddo
-    cxyz=cmu/ng_global/ng_global/ng_global
-    call pencil_fft_backward
-    open(11,file=output_name('mu3d'),status='replace',access='stream')
-    write(11) r3
-    close(11)
-    ! angular average (single node)
-    tpcf=0
-    do k=1,ng
-    do j=1,ng
-    do i=1,ng
-      i_dist=mod((/i,j,k/)+ng/2-1,ng)-ng/2
-      r_dist=sqrt(1.0*sum(i_dist**2))
-      ibin=nint(r_dist)+1
-      tpcf(1,ibin)=tpcf(1,ibin)+1
-      tpcf(2,ibin)=tpcf(2,ibin)+r_dist
-      tpcf(3,ibin)=tpcf(3,ibin)+r3(i,j,k)
-    enddo
-    enddo
-    enddo
-    tpcf(2,:)=tpcf(2,:)/tpcf(1,:)*box/ng
-    tpcf(3,:)=tpcf(3,:)/tpcf(1,:)
-    open(11,file=output_name('mu1d'),status='replace',access='stream')
-    write(11) n_dist,tpcf
-    close(11)
-    print*, 'called write_spin_and_stop'
-    stop
-  endsubroutine
-
-  subroutine correlate_spin(r_small,ratio_scale)
-    implicit none
-    save
-    real r_small,ratio_scale
-    call gaussian_fourier_filter(phi_k,r_small)
-    call pencil_fft_backward
-    phi(1:nf,1:nf,1:nf)=r3
-    call buffer_1layer(phi)
-    call gaussian_fourier_filter(phi_k,r_small*ratio_scale)
-    call pencil_fft_backward
-    phi_large(1:nf,1:nf,1:nf)=r3
-    call buffer_1layer(phi_large)
     call spinfield
     do ihalo=1,nhalo
       theta(1,ihalo)=ccc(spin_t(:,ihalo),spin(:,ind(1,ihalo),ind(2,ihalo),ind(3,ihalo)))
@@ -272,12 +387,12 @@ contains
     print*, r_small, ratio_scale, sum(theta,2)/nhalo
   endsubroutine
 
-  real function ccc(vec1,vec2)
+  real function ccc(vec_i,vec2)
     implicit none
-    real vec1(3),vec2(3)
-    vec1=vec1/norm2(vec1)
+    real vec_i(3),vec2(3)
+    vec_i=vec_i/norm2(vec_i)
     vec2=vec2/norm2(vec2)
-    ccc=sum(vec1*vec2)
+    ccc=sum(vec_i*vec2)
   endfunction
 
   subroutine buffer_1layer(phi)
@@ -371,7 +486,7 @@ contains
     do j=1,ng
     do i=1,ng
       i_dist=mod((/i,j,k/)+ng/2-1,ng)-ng/2
-      r_dist=sqrt(1.0*sum(i_dist**2))*box/ng
+      r_dist=norm2(real(i_dist))*box/ng
       r3(i,j,k)=merge(1.0,0.0,r_dist<=r_filter)
     enddo
     enddo
